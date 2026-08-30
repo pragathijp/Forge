76c5a2f694b9b693e822ecf0cd68d2c1f4ebd50b
pragathi_jp <pragathijpastay18@gmail.com>
2026-08-30 11:46:51 +0530

    Fix: resolve ESLint errors in requireAuth.ts type augmentation

--- src/middleware/requireAuth.ts
+++ src/middleware/requireAuth.ts
@@ -3,11 +3,13 @@ import passport from '../config/passport';
 import { UnauthorizedError } from '../utils/errors';
 import { TokenPayload } from '../services/tokenService';
 
+/* eslint-disable @typescript-eslint/no-namespace, @typescript-eslint/no-empty-object-type */
 declare global {
   namespace Express {
     interface User extends TokenPayload {}
   }
 }
+/* eslint-enable @typescript-eslint/no-namespace, @typescript-eslint/no-empty-object-type */
 
 export function requireAuth(req: Request, res: Response, next: NextFunction) {
   passport.authenticate('jwt', { session: false }, (err: unknown, user: TokenPayload | false) => {
